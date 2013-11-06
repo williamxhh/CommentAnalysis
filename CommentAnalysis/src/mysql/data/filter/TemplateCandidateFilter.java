a70628b3e22e6e896d3387784504aa3fed87e66c
williamxhh <gaoxiaowei1990@gmail.com>
2013-11-06 22:29:39 +0800

    抽取模板再调优，考虑了中间夹杂英文的可能性，但是过滤掉了仅出现英文的情况

--- CommentAnalysis/src/mysql/data/filter/TemplateCandidateFilter.java
+++ CommentAnalysis/src/mysql/data/filter/TemplateCandidateFilter.java
@@ -13,14 +13,15 @@ import java.util.regex.Pattern;
  */
 public class TemplateCandidateFilter{
 //	private String regex = "^(.*[\\pP|a-zA-Z|\\s|0-9])(.*)$";
-	private String regex = "[a-zA-Z|\\s|0-9|\\pP]*([^\\pPa-zA-Z\\s0-9]+)[:|£º]";
+	private String regex = "[a-zA-Z|\\s|0-9|\\pP]*([^\\pP\\s0-9]+)[:|£º]";
 
 	public List<String> getText(String inputStr) {
 		List<String> cands = new ArrayList<String>();
 		inputStr = inputStr.trim();
 		Matcher m = Pattern.compile(regex).matcher(inputStr);
 		while(m.find()){
-			cands.add(m.group(1));
+			if(!Pattern.matches("^[a-zA-Z]*$", m.group(1)))
+				cands.add(m.group(1));
 		}
 		return cands;
 	}
